13368d51afc6d2c32931a844e2e4a20ea4b66c0e
zeuspsy0x1 <zeusvargas26@gmail.com>
2023-08-26 03:52:59 -0500

    Feat: fluid scroll on navigation and sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import Home from '@/sections/home'
+import Chaquetas from '@/sections/chaquetas'
+import Caracteristicas from '@/sections/caracteristicas'
+import Comprar from '@/sections/comprar'
 
 export default function app() {
-	return <Home />
+	return (
+		<>
+			<Home />
+			<Chaquetas />
+			<Caracteristicas />
+			<Comprar />
+		</>
+	)
 }
--- src/components/navigation/navigationMenuMobile.tsx
+++ src/components/navigation/navigationMenuMobile.tsx
@@ -2,14 +2,15 @@
 
 import React, { useState } from 'react'
 import Image from 'next/image'
-import hamburgerIcon from '@/assets/hamburger-icon.png'
-import closeIcon from '@/assets/x-icon.png'
+import { Link } from 'react-scroll'
+import hamburgerIcon from '@/assets/hamburger-icon.svg'
+import closeIcon from '@/assets/x-icon.svg'
 
 const NavigationMenuMobile = () => {
 	const [showNavigation, setShowNavigation] = useState(true)
 
 	return (
-		<div className='absolute z-50'>
+		<div className='fixed z-50'>
 			{showNavigation ? (
 				<div className='flex w-[100vw]  mt-6 justify-end'>
 					<div
@@ -21,14 +22,81 @@ const NavigationMenuMobile = () => {
 					</div>
 				</div>
 			) : (
-				<div className='flex w-[100vw] h-[100vh] bg-[var(--body-background-color)] justify-end'>
-					<div
-						className='mt-6 mr-5'
-						onClick={() => {
-							setShowNavigation((booleanState) => !booleanState)
-						}}>
-						<Image src={closeIcon} alt='Close Icon' />
+				<div className='w-[100vw] h-[100vh] bg-[var(--body-background-color)]'>
+					<div className='flex  justify-end'>
+						<div
+							className='mt-6 mr-5 h-12 overflow-visible'
+							onClick={() => {
+								setShowNavigation((booleanState) => !booleanState)
+							}}>
+							<Image src={closeIcon} alt='Close Icon' />
+						</div>
 					</div>
+
+					{/* Nav menu */}
+
+					<ol className='flex justify-center items-center flex-col text-var(--light-gray-tex) text-xl  font-semibold py-2 mt-5'>
+						<li className='mt-4 p-2'>
+							<Link
+								onClick={() => {
+									setShowNavigation((booleanState) => !booleanState)
+								}}
+								to='home'
+								target='_blank'
+								spy={true}
+								smooth={true}
+								offset={-100}
+								duration={420}
+								className='cursor-pointer m-4 p-1 text-4xl font-light'>
+								Inicio
+							</Link>
+						</li>
+						<li className='mt-4 p-2 '>
+							<Link
+								onClick={() => {
+									setShowNavigation((booleanState) => !booleanState)
+								}}
+								to='chaquetas'
+								target='_blank'
+								spy={true}
+								smooth={true}
+								offset={-100}
+								duration={420}
+								className='cursor-pointer m-4 p-1 text-4xl font-light'>
+								Chaquetas
+							</Link>
+						</li>
+						<li className='mt-4 p-2'>
+							<Link
+								onClick={() => {
+									setShowNavigation((booleanState) => !booleanState)
+								}}
+								to='caracteristicas'
+								target='_blank'
+								spy={true}
+								smooth={true}
+								offset={-100}
+								duration={420}
+								className='cursor-pointer m-4 p-1 text-4xl font-light'>
+								Características
+							</Link>
+						</li>
+						<li className='mt-4 p-2'>
+							<Link
+								onClick={() => {
+									setShowNavigation((booleanState) => !booleanState)
+								}}
+								to='comprar'
+								target='_blank'
+								spy={true}
+								smooth={true}
+								offset={-100}
+								duration={420}
+								className='cursor-pointer m-4 p-1 text-4xl font-light'>
+								Comprar
+							</Link>
+						</li>
+					</ol>
 				</div>
 			)}
 		</div>
