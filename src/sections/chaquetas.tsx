ec87f124eac5e627c9e7d77e67679fe8bcef896d
zeuspsy0x1 <zeusvargas26@gmail.com>
2023-09-05 02:52:57 -0500

    Chaquetas Feature

--- src/sections/chaquetas.tsx
+++ src/sections/chaquetas.tsx
@@ -1,16 +1,18 @@
+import ImagesGallery from '@/components/images/chaquetasGallery/imagesGallery'
 import React from 'react'
 
 const Chaquetas = () => {
 	return (
-		<section id='chaquetas' className='h-[100vh]'>
+		<section id='chaquetas' className='md:mt-56 xl:hidden'>
 			<div className='grid place-items-center'>
 				<div className='flex flex-col items-end'>
-					<div className='text-5xl'>Chaquetas</div>
-					<div className='text-xl' style={{ color: 'var(--red-letters-color)' }}>
+					<h2 className='text-5xl lg:text-7xl '>Chaquetas</h2>
+					<span className='text-xl lg:text-3xl ' style={{ color: 'var(--red-letters-color)' }}>
 						2023
-					</div>
+					</span>
 				</div>
 			</div>
+			<ImagesGallery />
 		</section>
 	)
 }
