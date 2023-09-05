3e7118c8476cd6d4adb7a93e8a47477561624af5
zeuspsy0x1 <zeusvargas26@gmail.com>
2023-09-05 02:53:11 -0500

    características

--- src/sections/caracteristicas.tsx
+++ src/sections/caracteristicas.tsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import Image from 'next/image'
+import round1 from '@/assets/round-1.png'
+import round2 from '@/assets/round-2.png'
+import round3 from '@/assets/round-3.png'
+import round4 from '@/assets/round-4.png'
 
 const Caracteristicas = () => {
+	const roundCharacteristics = [
+		{
+			logo: 'Logo reflectivo',
+			image: round1,
+		},
+		{
+			logo: 'Puño con resorte',
+			image: round2,
+		},
+		{
+			logo: 'Espaldar reflectivo',
+			image: round3,
+		},
+		{
+			logo: 'Cremalleras antifluido',
+			image: round4,
+		},
+	]
+
+	const mappedCharacteristics = roundCharacteristics?.map(({ logo, image }, i) => {
+		return (
+			<div className='grid place-items-center '>
+				<div className='h-[256px] w-[256px] max-h-[256px] max-w-[256px] border-neutral-700'>
+					<div
+						key={i}
+						className='border border-neutral-700 rounded-full overflow-hidden'
+						style={{
+							width: '256px',
+							height: '256px',
+						}}>
+						<Image src={image} alt='Jacket characteristic image' />
+					</div>
+				</div>
+				<span key={i} className='text-2xl mb-8 mt-4 lg:mb-16 lg:mt-2 lg:text-3xl'>
+					{logo}
+				</span>
+			</div>
+		)
+	})
+
 	return (
-		<section id='caracteristicas' className='h-[100vh]'>
-			<div className='h-[100vh]'>caracteristicas</div>
+		<section id='caracteristicas' className='mt-20 xl:hidden'>
+			<div className='grid place-items-center'>
+				<h2 className=' text-5xl lg:text-7xl '>Características</h2>
+			</div>
+			<div className='mt-12'>{mappedCharacteristics}</div>
 		</section>
 	)
 }
---
+++ src/sections/caracteristicasDesktop.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from 'next/image'
+import caracteristicasImage from '@/assets/paw.png'
+
+const CaracteristicasDesktop = () => {
+	return (
+		<div id='chaquetas' className='hidden lg:block mt-20'>
+			<div className='grid place-items-center'>
+				<h2 className='text-5xl lg:text-7xl mb-4'>Características</h2>
+
+				<div className='p-2'>
+					<Image
+						src='https://res.cloudinary.com/dnw4kirdp/image/upload/v1693895717/Landing%20Pages/Dexterous/paw_ycyl5k.png'
+						quality={100}
+						alt='jacket image'
+						/* fill={true} */
+						width={1500}
+						height={100}
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default CaracteristicasDesktop
